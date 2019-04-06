6e5ea218a8b0863001b7bc95ab7d325ba8641533
RoySharad <sharad199994@gmail.com>
2019-04-06 12:26:53 +0530

    Committing processService and pom for mialing method and dependency

--- src/main/java/com/cloud9/onboard/camunda/service/ProcessService.java
+++ src/main/java/com/cloud9/onboard/camunda/service/ProcessService.java
@@ -1,7 +1,19 @@
 package com.cloud9.onboard.camunda.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import com.cloud9.onboard.camunda.request.StartProcessWithFormRequest;
 import com.cloud9.onboard.camunda.response.ProcessDefinitionResponse;
@@ -13,5 +25,39 @@ public interface ProcessService {
 	public StartProcessWithFormResponse startProcessWithForm(StartProcessWithFormRequest request,String processDefinitionId);
 	public Map<String,FieldValue> getStartFormVariables(String processDefinitionId);
 	List<ProcessDefinitionResponse> getProcessDefinitions();
+	public static  void sendmail(String receiverMailId, String name) {
+		
+		try{
+		   Properties props = new Properties();
+		   props.put("mail.smtp.auth", "true");
+		   props.put("mail.smtp.starttls.enable", "true");
+		   props.put("mail.smtp.host", "smtp.gmail.com");
+		   props.put("mail.smtp.port", "587");
+		   
+		   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+		      protected PasswordAuthentication getPasswordAuthentication() {
+		         return new PasswordAuthentication("sharad199994@gmail.com", "roy_1994");
+		      }
+		   });
+		   String messageContent="Welcome "+name.toUpperCase();
+		    Message msg = new MimeMessage(session);
+		   msg.setFrom(new InternetAddress("sharad199994@gmail.com", false));
 
+		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverMailId));
+		   msg.setSubject("On-boarding");
+		   msg.setContent("On-boarding", "text/html");
+		   msg.setSentDate(new Date());
+
+		   MimeBodyPart messageBodyPart = new MimeBodyPart();
+		   messageBodyPart.setContent(messageContent, "text/html");
+
+		   Multipart multipart = new MimeMultipart();
+		   multipart.addBodyPart(messageBodyPart);
+		   msg.setContent(multipart);
+		   Transport.send(msg);   
+		}
+		catch(Exception e){
+			
+		}
+	}
 }
