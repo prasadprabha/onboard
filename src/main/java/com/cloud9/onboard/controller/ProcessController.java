60ce1a60e4b739f009fa35bcc8bb5c1f844e4ba8
Prasad <prasadprabha07@gmail.com>
2019-04-06 09:10:03 +0530

    changes

--- src/main/java/com/cloud9/onboard/controller/ProcessController.java
+++ src/main/java/com/cloud9/onboard/controller/ProcessController.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cloud9.onboard.camunda.request.StartProcessWithFormRequest;
 import com.cloud9.onboard.camunda.response.ProcessDefinitionResponse;
+import com.cloud9.onboard.camunda.response.StartProcessWithFormResponse;
 import com.cloud9.onboard.camunda.service.ProcessService;
 import com.cloud9.onboard.pojo.FieldValue;
 import com.cloud9.onboard.pojo.FormField;
@@ -42,14 +44,38 @@ public class ProcessController {
 	}
 	
 	@RequestMapping(value = "/get-process-definitions", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Map<String,String> getProcessDefinitons() {
-		Map<String,String> processMap = new HashMap<String,String>();
+	public FormField[] getProcessDefinitons() {
+		int index = 0;
 		List<ProcessDefinitionResponse>  processDefintions = processService.getProcessDefinitions();
+		FormField[] formFieldArry = new FormField[processDefintions.size()];
 		for (ProcessDefinitionResponse response : processDefintions) {
-			processMap.put(response.getName(), response.getId());
+			FormField formField = new FormField();
+			formField.setControlName(response.getName());
+			formField.setControlType(response.getId());
+			formFieldArry[index++] = formField;
+			
 		}
 		
-		return processMap;
+		return formFieldArry;
+	}
+	
+	@RequestMapping(value = "/start-process-from", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE) 
+	public StartProcessWithFormResponse startProcess(Map<String,String> variables,String processDefinitionId) {
+		StartProcessWithFormRequest startProcessWithFormRequest = new StartProcessWithFormRequest();
+		Map<String, HashMap<String, String>> variableMap  = startProcessWithFormRequest.getVariables();
+		//String processDefinitionId = "";
+		
+		for (Map.Entry<String,String> entry : variables.entrySet()) {
+			HashMap<String,String> firstName = new HashMap<String,String>();
+			firstName.put("value", entry.getValue());
+			firstName.put("type", "String");
+			if(entry.getKey().equals("email")) {
+				startProcessWithFormRequest.setBusinessKey(entry.getValue());
+			}
+			variableMap.put(entry.getKey(), firstName);
+		}
+		StartProcessWithFormResponse response = processService.startProcessWithForm(startProcessWithFormRequest, processDefinitionId);
+		return response;
 	}
 
 }
